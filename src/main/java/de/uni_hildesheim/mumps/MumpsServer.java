91eb575792fb7a7021dccaba180fc859161db778
Adam Krafczyk <krafczyk@uni-hildesheim.de>
2025-06-27 17:55:12 +0200

    Initial project structure

---
+++ src/main/java/de/uni_hildesheim/mumps/MumpsServer.java
@@ -0,0 +1,13 @@
+package de.uni_hildesheim.mumps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumpsServer {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(MumpsServer.class, args);
+    }
+
+}
