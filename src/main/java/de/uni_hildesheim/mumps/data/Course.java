0f5857d6952d03a695fc1c401b8c4bde24765d79
Adam Krafczyk <krafczyk@uni-hildesheim.de>
2025-06-27 18:17:35 +0200

    Add basic data structure

---
+++ src/main/java/de/uni_hildesheim/mumps/data/Course.java
@@ -0,0 +1,63 @@
+package de.uni_hildesheim.mumps.data;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    
+    private String name;
+    
+    private int rewardPerEvent;
+    
+    private List<Event> events;
+    
+    // for JPA
+    protected Course() {
+    }
+    
+    public Course(String name) {
+        this.name = name;
+        this.rewardPerEvent = 1;
+        this.events = new LinkedList<>();
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getRewardPerEvent() {
+        return rewardPerEvent;
+    }
+    
+    public void setRewardPerEvent(int rewardPerEvent) {
+        this.rewardPerEvent = rewardPerEvent;
+    }
+    
+    public void addEvent(Event event) {
+        events.add(event);
+    }
+    
+    public List<Event> getEvents() {
+        return Collections.unmodifiableList(events);
+    }
+    
+    // for JPA?
+    public void setEvents(List<Event> events) {
+        this.events = events;
+    }
+    
+}
---
+++ src/main/java/de/uni_hildesheim/mumps/data/Event.java
@@ -0,0 +1,26 @@
+package de.uni_hildesheim.mumps.data;
+
+import java.time.ZonedDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    
+    private ZonedDateTime startTime;
+    
+    // for JPA
+    protected Event() {
+    }
+
+    public Event(ZonedDateTime startTime) {
+        this.startTime = startTime;
+    }
+    
+}
